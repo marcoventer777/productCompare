55ea9b05a97a37ae4507bf6142d1ef840f1d819c
Marco Venter <marcov@bbd.co.za>
2022-07-12 17:43:09 +0200

    CSP index.html configured

--- frontend/src/app/components/products/product.module.ts
+++ frontend/src/app/components/products/product.module.ts
@@ -6,12 +6,13 @@ import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 import { AuthGuard } from '@auth0/auth0-angular';
+import { SafePipe } from 'src/app/trust.pipe';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
+    ConvertToSpacesPipe
   ],
   imports: [
     RouterModule.forChild([
